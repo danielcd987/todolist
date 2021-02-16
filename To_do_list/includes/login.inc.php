27362d7db8940a486409da6de667fa010a53844d
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2021-02-16 17:07:34 -0500

    login and signup

--- To_do_list/includes/login.inc.php
+++ To_do_list/includes/login.inc.php
@@ -1,14 +1,58 @@
 <?php
-$username = $_POST['username'];
-$password = $_POST['password'];
-
 if(isset($_POST['login'])){
-    if($username == "dan"){
-        echo"TRUE";
+        
+    require_once 'tdDbc.php';
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if(empty($username) || empty($password)){
+        header("Location: ../index.php?error=emptyfields");
+        exit();
     }
     else{
-        echo"FALSE";
-    }
+        $sql = "SELECT * FROM users WHERE user_names = ? OR email = ?;";
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt,$sql)){
+            header("Location: ../index.php?error=loginfailedsql");
+            exit();
+        }
+        else{
+            mysqli_stmt_bind_param($stmt, "ss", $username,$username);
+            mysqli_stmt_execute($stmt);
+
+            $result = mysqli_stmt_get_result($stmt);
+            
+            if($rows = mysqli_fetch_assoc($result)){
+                $checkpassword = password_verify($password, $rows["pwd"]);
+                if($checkpassword == false){
+                    header("Location: ../index.php?error=incorrectpassword");
+                    exit();
+                }
+                else if($checkpassword == true){
+                    session_start();
+                    $_SESSION['id'] = $row['id']; 
+                    $_SESSION['user_name'] = $row['user_name']; 
+                    $_SESSION['first_name'] = $row['first_name']; 
+                    $_SESSION['last_name'] = $row['last_name'];
+                    $_SESSION['email'] = $row['email']; 
+                   header("Location: ../tasks_list.php?login=success");
+                   exit();  
+
+                }
+            }
+            else{
+                header("Location: ../index.php?error=wrongpassword");
+                exit();
+            }
+        }
+    } 
+
 }
+else{
+    header("Location: ../index.php");
+    exit();
+}
+
 
 ?>
--- To_do_list/sign-up.php
+++ To_do_list/sign-up.php
@@ -6,6 +6,39 @@
 
 <br><h2 class = "heading">Sign Up:</h2>
 
+<?php
+if(isset($_GET["error"])){
+    if($_GET["error"] == "emptyfields"){
+        echo"<h3 class = 'error'>ERROR: Fill in Every Field</h3>";
+    }
+    else if($_GET["error"] == "username"){
+        echo"<h3 class = 'error'>ERROR: Include No Spaces in User Name.</h3>";
+    }
+    else if($_GET["error"] == "email.uname"){
+        echo"<h3 class = 'error'>ERROR: Enter valid Email and User Name.</h3>";
+    }
+    else if($_GET["error"] == "email"){
+        echo"<h3 class = 'error'>ERROR: Enter Valid Email</h3>";
+    }
+    else if($_GET["error"] == "username_taken"){
+        echo"<h3 class = 'error'>ERROR: User Name Taken</h3>";
+    }
+    else if($_GET["error"] == "invaliduserid"){
+        echo"<h3 class = 'error'>ERROR: Enter Valid User Name</h3>";
+    }
+    else if($_GET["error"] == "password_nomatch"){
+        echo"<h3 class = 'error'>ERROR: Passwords Dont Match</h3>";
+    }
+    else if($_GET["error"] == "sqlerrors"){
+        echo"<h3 class = 'error'>ERROR: An Error Has Occurred Please Try Again.</h3>";
+    }
+    else if($_GET["error"] == "noerrors"){
+        echo"<h3 class = 'error'>Success: Your Account Was Created Successfully</h3>";
+    }
+}
+
+?>
+
 <form name = "signup" action = "includes/signup.inc.php" method = "POST">
     
     <h3 class = "heading">First Name:</h3> 
