0263369a19c3e1b79851771dbd7e7994297cc7aa
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2021-04-22 09:06:47 -0400

    trying to figure out why login doesnt work

--- To_do_list/includes/login.inc2.php
+++ To_do_list/includes/login.inc2.php
@@ -10,7 +10,8 @@
             exit();
         }
         else{
-            $sql = "SELECT * FROM users WHERE user_names =? OR email = ?;";
+            // $sql = "SELECT * FROM users WHERE user_names =? OR email = ?;";
+            $sql = "SELECT * FROM users WHERE user_names =?;";
             $stmt = mysqli_stmt_init($conn);
             if(!mysqli_stmt_prepare($stmt,$sql)){
                 header("Location: ../index.php?error=sqlerror");
@@ -18,17 +19,17 @@
             }
             else{
 
-                mysqli_stmt_bind_param($stmt, "ss", $userEmail, $userEmail);
+                mysqli_stmt_bind_param($stmt, "s", $userEmail);
                 mysqli_stmt_execute($stmt);
 
                 $result = mysqli_stmt_get_result($stmt);
                 if($row = mysqli_fetch_assoc($result)){
-                    $paswdCheck = password_verify($passwd, $row['pwd']);
-                    if($paswdCheck == false){
+                    $paswdCheck = password_verify($paswd, $row['pwd']);
+                    if($paswdCheck === false){
                     header("Location: ../index.php?error=wrongpwd");
                     exit();
                     }
-                    else if($paswdCheck == true){
+                    else if($paswdCheck === true){
                         session_start();
                         $_SESSION['id_user'] = $row['id'];
                         $_SESSION['username'] = $row['user_names'];
