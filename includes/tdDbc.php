c512382abd30c57dd44801e1e4ee97bd8c810a43
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2021-09-10 11:23:48 -0400

    adding new db config

--- includes/tdDbc.php
+++ includes/tdDbc.php
@@ -1,9 +1,9 @@
 <?php
 //servernames and database names needed to connect
-$servername = "us-cdbr-east-04.cleardb.com";
-$dBUsername = "b66f47e4e41f6f";
-$dBPassword = "8a30a3ed";
-$dBName = "heroku_30e79bb25759c2d";
+// $servername = "us-cdbr-east-04.cleardb.com";
+// $dBUsername = "b66f47e4e41f6f";
+// $dBPassword = "8a30a3ed";
+// $dBName = "heroku_30e79bb25759c2d";
 
 //for xampp
 
@@ -13,24 +13,28 @@ $dBName = "heroku_30e79bb25759c2d";
 // $dBName = "activity";
 
 //connects database
-$conn = mysqli_connect($servername, $dBUsername, $dBPassword,$dBName);
+// $conn = mysqli_connect($servername, $dBUsername, $dBPassword,$dBName);
 
 
 //alerts if the connection failed
-if(!$conn){
-    die("Connection failed: " .mysqli_connect_error());
-}
+// if(!$conn){
+//     die("Connection failed: " .mysqli_connect_error());
+// }
 
 //connects the db to the heroku platform 
 //from the tutorial created by doable danny
 
 //Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"],1);
-// $active_group = 'default';
-// $query_builder = TRUE;
-// // Connect to DB
-// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+if(!$conn){
+    die("Connection failed: " .mysqli_connect_error());
+}
