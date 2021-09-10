0ada73d302d753d97200132292493979486395e6
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2021-09-10 10:48:39 -0400

    db edits

--- includes/tdDbc.php
+++ includes/tdDbc.php
@@ -1,9 +1,15 @@
 <?php
 //servernames and database names needed to connect
-$servername = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "activity";
+$servername = "us-cdbr-east-04.cleardb.com";
+$dBUsername = "b66f47e4e41f6f";
+$dBPassword = "8a30a3ed";
+$dBName = "heroku_30e79bb25759c2d";
+
+
+// $servername = "localhost";
+// $dBUsername = "root";
+// $dBPassword = "";
+// $dBName = "activity";
 
 //connects database
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword,$dBName);
@@ -18,12 +24,12 @@ if(!$conn){
 //from the tutorial created by doable danny
 
 //Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+// $cleardb_server = $cleardb_url["host"];
+// $cleardb_username = $cleardb_url["user"];
+// $cleardb_password = $cleardb_url["pass"];
+// $cleardb_db = substr($cleardb_url["path"],1);
+// $active_group = 'default';
+// $query_builder = TRUE;
+// // Connect to DB
+// $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
