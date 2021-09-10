95b8db7ee0f6c298c42d1f89fb5ae31937dddd34
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2021-09-10 10:33:45 -0400

    adding db deployment on heroku

--- includes/tdDbc.php
+++ includes/tdDbc.php
@@ -13,3 +13,17 @@ $conn = mysqli_connect($servername, $dBUsername, $dBPassword,$dBName);
 if(!$conn){
     die("Connection failed: " .mysqli_connect_error());
 }
+
+//connects the db to the heroku platform 
+//from the tutorial created by doable danny
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
